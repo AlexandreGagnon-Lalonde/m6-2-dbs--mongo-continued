7a84d516bf6ef8f7ec4f4bd170b4f2ee0ac66c7b
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-10-03 13:47:46 -0400

    update seat booking on mongo

--- server/handlers.js
+++ server/handlers.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const fs = require("file-system");
 const { MongoClient } = require("mongodb");
@@ -8,39 +8,62 @@ const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
+const assert = require("assert");
 
 const getSeats = async (req, res) => {
   const client = await MongoClient(MONGO_URI, options);
   try {
     await client.connect();
 
-    const db = client.db('6-2--exercise_1');
+    const db = client.db("6-2--exercise_1");
 
-    const seats = await db.collection('seats').find().toArray();
+    const seats = await db.collection("seats").find().toArray();
 
     if (seats.length === 0) {
-      res.status(404).json({ status: 404, message: 'No data' });
+      res.status(404).json({ status: 404, message: "No data" });
     } else {
-      res.status(200).json({ status: 200, data: seats});
+      res.status(200).json({ status: 200, data: seats });
     }
   } catch (err) {
-    res.status(500).json({ status: 500, message: err.message })
+    res.status(500).json({ status: 500, message: err.message });
   }
   client.close();
 };
 
 const updateSeat = async (req, res) => {
   const client = await MongoClient(MONGO_URI, options);
+
   try {
+    // find id of seat to update
+    const { seatId } = req.body;
+    // create query with mongo property
+    const query = { _id: seatId };
+    // value for true and false
+    const newValueTrue = { $set: { isBooked: true } };
+    const newValueFalse = { $set: { isBooked: false } };
+    // connect client
     await client.connect();
-
-    const db = client.db('6-2--exercise_1');
-
-    
+    // find good database
+    const db = client.db("6-2--exercise_1");
+    // find seat to update in databse
+    const currentSeat = await db.collection("seats").findOne({ _id: seatId });
+    // create temp seat variable
+    let seat;
+    // check if seat isBooked or not
+    if (currentSeat.isBooked) {
+      seat = await db.collection("seats").updateOne(query, newValueFalse);
+    } else {
+      seat = await db.collection("seats").updateOne(query, newValueTrue);
+    }
+    // check that value has been updated
+    assert.equal(1, seat.matchedCount);
+    assert.equal(1, seat.modifiedCount);
+    // server response
+    res.status(204).json({ status: 204, data: currentSeat });
   } catch (err) {
-    res.status(500).json({ status: 500, message: err.message });
+    res.status(500).json({ status: 500, message: err });
   }
   client.close();
-}
+};
 
-module.exports = { getSeats };
+module.exports = { getSeats, updateSeat };
--- server/routes.js
+++ server/routes.js
@@ -10,7 +10,7 @@
 // // MONGO ^^
 
 const router = require("express").Router();
-const { getSeats } = require('./handlers');
+const { getSeats, updateSeat } = require("./handlers");
 
 const NUM_OF_ROWS = 8;
 const SEATS_PER_ROW = 12;
@@ -63,48 +63,7 @@ router.get("/api/seat-availability", getSeats);
 
 let lastBookingAttemptSucceeded = false;
 
-router.post("/api/book-seat", async (req, res) => {
-  const { seatId, creditCard, expiration } = req.body;
-
-  if (!state) {
-    state = {
-      bookedSeats: randomlyBookSeats(30),
-    };
-  }
-
-  await delay(Math.random() * 3000);
-
-  const isAlreadyBooked = !!state.bookedSeats[seatId];
-  if (isAlreadyBooked) {
-    return res.status(400).json({
-      message: "This seat has already been booked!",
-    });
-  }
-
-  if (!creditCard || !expiration) {
-    return res.status(400).json({
-      status: 400,
-      message: "Please provide credit card information!",
-    });
-  }
-
-  if (lastBookingAttemptSucceeded) {
-    lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
-
-    return res.status(500).json({
-      message: "An unknown error has occurred. Please try your request again.",
-    });
-  }
-
-  lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
-
-  state.bookedSeats[seatId] = true;
-
-  return res.status(200).json({
-    status: 200,
-    success: true,
-  });
-});
+router.post("/api/book-seat", updateSeat);
 
 module.exports = router;
 
@@ -112,7 +71,7 @@ module.exports = router;
 //   const client = await MongoClient(MONGO_URI, options);
 //   try {
 //     await client.connect();
-  // poor choice of databse name... 
+// poor choice of databse name...
 //     const db = client.db("6-2--exercise_1");
 
 //     const dataSeats = await db.collection("seats").insertMany(seatsArray);
