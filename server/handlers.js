4334af2e4d51054f0be74721f3bda2861af43d50
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-10-04 20:19:23 -0400

    add user to seats database, and new users db

--- server/handlers.js
+++ server/handlers.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const fs = require("file-system");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const { MONGO_URI } = process.env;
@@ -15,14 +14,22 @@ const getSeats = async (req, res) => {
   try {
     await client.connect();
 
-    const db = client.db("6-2--exercise_1");
+    const db = client.db("6-2");
 
     const seats = await db.collection("seats").find().toArray();
+
+    const response = {};
+
+    seats.forEach((seat) => {
+      response[seat._id] = seat;
+    });
 
     if (seats.length === 0) {
       res.status(404).json({ status: 404, message: "No data" });
     } else {
-      res.status(200).json({ status: 200, data: seats });
+      res
+        .status(201)
+        .json({ status: 201, seats: response, numOfRows: 8, seatsPerRow: 12 });
     }
   } catch (err) {
     res.status(500).json({ status: 500, message: err.message });
@@ -35,33 +42,43 @@ const updateSeat = async (req, res) => {
 
   try {
     // find id of seat to update
-    const { seatId } = req.body;
-    // create query with mongo property
-    const query = { _id: seatId };
-    // value for true and false
-    const newValueTrue = { $set: { isBooked: true } };
-    const newValueFalse = { $set: { isBooked: false } };
-    // connect client
-    await client.connect();
-    // find good database
-    const db = client.db("6-2--exercise_1");
-    // find seat to update in databse
-    const currentSeat = await db.collection("seats").findOne({ _id: seatId });
-    // create temp seat variable
-    let seat;
-    // check if seat isBooked or not
-    if (currentSeat.isBooked) {
-      seat = await db.collection("seats").updateOne(query, newValueFalse);
+    const { seatId, expiration, creditCard, email, fullName } = req.body;
+    // check if form is filled
+    if (!expiration || !creditCard || !email || !fullName) {
+      res.status(404).json({ status: 404, message: "Please fill the form" });
     } else {
-      seat = await db.collection("seats").updateOne(query, newValueTrue);
+      // create query with mongo property
+      const query = { _id: seatId };
+      // value for true and false
+      const newValueTrue = { $set: { isBooked: true, fullName, email } };
+      const newValueFalse = { $set: { isBooked: false } };
+      // connect client
+      await client.connect();
+      // find good database
+      const db = client.db("6-2");
+      // add new user in users database
+      const newUser = await db
+        .collection("users")
+        .insertOne({ fullName, email });
+      assert.equal(1, newUser.insertedCount);
+      // find seat to update in databse
+      const currentSeat = await db.collection("seats").findOne({ _id: seatId });
+      // create temp seat variable
+      let seat;
+      // check if seat isBooked or not
+      if (currentSeat.isBooked) {
+        seat = await db.collection("seats").updateOne(query, newValueFalse);
+      } else {
+        seat = await db.collection("seats").updateOne(query, newValueTrue);
+      }
+      // check that value has been updated
+      assert.equal(1, seat.matchedCount);
+      assert.equal(1, seat.modifiedCount);
+      // server response
+      res.status(204).json({ status: 204, success: true });
     }
-    // check that value has been updated
-    assert.equal(1, seat.matchedCount);
-    assert.equal(1, seat.modifiedCount);
-    // server response
-    res.status(204).json({ status: 204, data: currentSeat });
   } catch (err) {
-    res.status(500).json({ status: 500, message: err });
+    res.status(500).json({ status: 500, message: err.message });
   }
   client.close();
 };
--- server/routes.js
+++ server/routes.js
@@ -1,87 +1,61 @@
-// const fs = require("file-system");
-// const { MongoClient } = require("mongodb");
-// require("dotenv").config();
-// const { MONGO_URI } = process.env;
-// const options = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// };
-// const assert = require("assert");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+const assert = require("assert");
 // // MONGO ^^
 
 const router = require("express").Router();
 const { getSeats, updateSeat } = require("./handlers");
 
-const NUM_OF_ROWS = 8;
-const SEATS_PER_ROW = 12;
-
-// // Code that is generating the seats.
-// // ----------------------------------
-// const seats = {};
-// const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
-// for (let r = 0; r < row.length; r++) {
-//   for (let s = 1; s < 13; s++) {
-//     seats[`${row[r]}-${s}`] = {
-//       price: 225,
-//       isBooked: false,
-//     };
-//   }
-// }
-// const seatsArray = [];
-// Object.keys(seats).forEach((seat) => {
-//   seatsArray.push({
-//     _id: seat,
-//     ...seats[seat],
-//   });
-// });
-// // ----------------------------------
-//////// HELPERS
-const getRowName = (rowIndex) => {
-  return String.fromCharCode(65 + rowIndex);
-};
-
-const randomlyBookSeats = (num) => {
-  const bookedSeats = {};
-
-  while (num > 0) {
-    const row = Math.floor(Math.random() * NUM_OF_ROWS);
-    const seat = Math.floor(Math.random() * SEATS_PER_ROW);
-
-    const seatId = `${getRowName(row)}-${seat + 1}`;
-
-    bookedSeats[seatId] = true;
-
-    num--;
+// Code that is generating the seats.
+// ----------------------------------
+const seats = {};
+const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
+for (let r = 0; r < row.length; r++) {
+  for (let s = 1; s < 13; s++) {
+    seats[`${row[r]}-${s}`] = {
+      price: 225,
+      isBooked: false,
+    };
   }
-
-  return bookedSeats;
-};
-
-let state;
+}
+const seatsArray = [];
+Object.keys(seats).forEach((seat) => {
+  seatsArray.push({
+    _id: seat,
+    seatId: seat,
+    ...seats[seat],
+  });
+});
+console.log(seatsArray)
+// ----------------------------------
+//////// HELPERS
 
 router.get("/api/seat-availability", getSeats);
-
-let lastBookingAttemptSucceeded = false;
 
 router.post("/api/book-seat", updateSeat);
 
 module.exports = router;
 
-// const batchImport = async (req, res) => {
-//   const client = await MongoClient(MONGO_URI, options);
-//   try {
-//     await client.connect();
-// poor choice of databse name...
-//     const db = client.db("6-2--exercise_1");
+const batchImport = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
 
-//     const dataSeats = await db.collection("seats").insertMany(seatsArray);
-//     assert.equal(seatsArray.length, dataSeats.insertedCount);
+    const db = client.db("6-2");
 
-//     console.log("success");
-//   } catch (err) {
-//     console.log(err.message);
-//   }
-//   client.close();
-// };
+    const dataSeats = await db.collection("seats").insertMany(seatsArray);
+    assert.equal(seatsArray.length, dataSeats.insertedCount);
 
-// batchImport();
+    console.log("success");
+  } catch (err) {
+    console.log(err.message);
+  }
+  client.close();
+};
+
+batchImport();
